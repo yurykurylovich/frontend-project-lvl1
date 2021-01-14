6167c57620ecb6c1f3ba2c7a4525a158c254dbd6
yurykurylovich <yury.kurylovich@gmail.com>
2021-01-14 22:58:43 +0300

    Refactor esliint config and project code

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync'
 
 const greeting = () => {
-  let userName = readlineSync.question("May I have your name? ");
+  const userName = readlineSync.question("May I have your name? ");
   console.log(`Hello, ${userName}`);
 }
 
-export { greeting }
+export default greeting
